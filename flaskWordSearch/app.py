72ec2474131dc746a933fbeea93c7406380d9c1d
Paul Taylor <pltorigami@gmail.com>
2020-11-03 23:58:38 -0500

    created form to get URL of puzzle with validation

--- flaskWordSearch/app.py
+++ flaskWordSearch/app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+import time
+
+from flask import Flask, redirect, render_template, request, url_for
+
+from forms import urlForm
+from utils import findWords, getGridAndList, getHtml
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "M-{4;:66>_0E%ziF%y)#m<Rye#Y5&RU{[&24y,<9"
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "quick test ftb"
+    form = urlForm()
+    if form.validate_on_submit():
+        return redirect(url_for("wordSearchPage", url=form.url.data))
+    return render_template("form.html", form=form, timeStamp=time.time())
+
+
+@app.route("/puzzle")
+def wordSearchPage():
+    # return request.args.get("url")
+    pageContent = getHtml(request.args.get("url"))
+    fullGrid, wordsList = getGridAndList(pageContent)
+    # resultDict = findWords(fullGrid, wordsList)
+    return render_template("puzzle.html", grid=fullGrid, words=wordsList)
 
 
 if __name__ == "__main__":
--- flaskWordSearch/forms.py
+++ flaskWordSearch/forms.py
@@ -1,7 +1,26 @@
+import requests
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextField
+from requests.exceptions import ConnectionError
+from wtforms import StringField, SubmitField
+from wtforms.validators import ValidationError
 
 
 class urlForm(FlaskForm):
-    url = TextField(label="Enter URL here:")
-    submit = SubmitField(label="Submit")
+    url = StringField(label="Enter URL here:")
+    submit = SubmitField()
+
+    def validate_url(self, url):
+        if not (url.data.startswith("http://") or url.data.startswith("https://")):
+            goodPrefix = ""
+            for prefix in ["http://", "https://"]:
+                try:
+                    requests.get(prefix + url.data)
+                    goodPrefix = prefix
+                    break
+                except ConnectionError:
+                    pass
+
+            if not goodPrefix:
+                raise ValidationError("Please enter a valid URL.")
+            else:
+                url.data = goodPrefix + url.data
